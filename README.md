72b8804a14714994780575a0d1ebcc3e6efe90f9
dwncy <sw-dev1@formativeai.com>
2019-05-03 00:05:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# gucci-gang
+for fun
---
+++ index.js
@@ -0,0 +1,21 @@
+function gucciGang() {
+  console.log('Gucci Gang, Gucci Gang, Gucci Gang, Gucci Gang');
+}
+
+function partOne() {
+  console.log('Spend three racks on a new chain (Yuh)');
+  console.log('My bitch love do cocaine, ooh (Ooh)');
+  console.log('I fuck a bitch, I forgot her name (Brr, yuh)');
+  console.log('Can\'t buy a bitch no wedding ring');
+  console.log('Rather go and buy Balmains (Brr)');
+}
+
+function chorus() {
+  gucciGang();
+  gucciGang();
+  partOne();
+  gucciGang();
+  gucciGang();
+}
+
+module.exports = chorus;
